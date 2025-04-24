341c6023664a4d85eff0790ae428a9b73e377316
Adelanwa Oreofe <emmanueldamilare121@gmail.com>
2025-04-24 12:39:02 +0100

    /resume Requestmapping removed from resumeController

--- resume-analyzer/src/main/java/com/slickdev/resume_analyzer/web/ResumeController.java
+++ resume-analyzer/src/main/java/com/slickdev/resume_analyzer/web/ResumeController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/resume")
 public class ResumeController {
 
     ResumeServiceImpl resumeService;
