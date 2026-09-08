92afa5cad0d76c218d28ab2f064fece8f641e4b7
Adelanwa Oreofe <emmanueldamilare121@gmail.com>
2026-09-08 15:54:53 +0100

    updated ResumeAnalysisRepositoryIntegrationTest

--- resume-analyzer/src/test/java/com/slickdev/resume_analyzer/repository/ResumeAnalysisRepositoryIntegrationTest.java
+++ resume-analyzer/src/test/java/com/slickdev/resume_analyzer/repository/ResumeAnalysisRepositoryIntegrationTest.java
@@ -9,7 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.slickdev.resume_analyzer.entities.ResumeAnalysis;
 import com.slickdev.resume_analyzer.entities.UploadedResume;
@@ -19,13 +24,28 @@ import com.slickdev.resume_analyzer.repositories.ResumeAnalysisRepository;
 import com.slickdev.resume_analyzer.repositories.ResumeRepository;
 import com.slickdev.resume_analyzer.repositories.UserRepository;
 
+@Testcontainers 
 @SpringBootTest
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @Transactional
 class ResumeAnalysisRepositoryIntegrationTest {
     @Autowired private UserRepository userRepository;
     @Autowired private ResumeRepository resumeRepository;
     @Autowired private ResumeAnalysisRepository resumeAnalysisRepository;
+
+     @Container 
+    static PostgreSQLContainer<?> postgres =
+        new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void summaryQueryReturnsOnlyTheUsersAnalysesNewestFirst() {
